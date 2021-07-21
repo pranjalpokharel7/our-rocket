04da97bec7d8fa4d42890c66f34c44246350ff1e
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2021-07-22 02:33:55 +0545

    hello triangle, shader class

---
+++ includes/shader.h
@@ -0,0 +1,27 @@
+#ifndef SHADER_HPP_
+#define SHADER_HPP_
+
+#include "glad.h"
+#include <iostream>
+
+class Shader {
+private:
+  unsigned int programID;
+  GLuint vertexShader;
+  GLuint fragmentShader;
+  enum class entityType { V_SHADER, F_SHADER, PROGRAM };
+
+  char* loadShaderFromFile(const char *filePath);
+  void checkCompileError(GLuint s, entityType type);
+  unsigned long getFileLength(FILE *fp);
+
+public:
+  Shader();
+  ~Shader();
+  int loadVertexShader(const char *filePath);
+  int loadFragmentShader(const char *filePath);
+  int loadProgram();
+  void useGlProgram();
+};
+
+#endif // SHADER_H_
